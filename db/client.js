2b81b9f48f02c6b5366939ca634b70f69a3cb11c
Alia Taha <aliataha5538@gmail.com>
2022-08-10 17:26:31 -0500

    recovered db/client.js

--- db/client.js
+++ db/client.js
@@ -14,24 +14,3 @@ const client = new Pool({
 });
 
 module.exports = client;
-
-/*
-const DB_URL =
-  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
-
-let client;
-
-// github actions client config
-if (process.env.CI) {
-  client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'postgres',
-    port: 5432,
-  });
-} else {
-  // local / heroku client config
-  client = new Client(DB_URL);
-}
-*/
