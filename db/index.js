5d000cb8543b31a40ec91aab8fb9c14a069b398f
Alia Taha <aliataha5538@gmail.com>
2022-08-10 20:09:54 -0500

    db/index.js complete - cleared

--- db/index.js
+++ db/index.js
@@ -1,7 +1,11 @@
-const client = require('./client');
-const models = require('./models');
-
 module.exports = {
-  client,
-  ...models,
+  ...require('./client'),
+  ...require('./users'),
+  ...require('./product'),
+  ...require('./cart'),
+  ...require('./cart_products'),
+  ...require('./orders'),
 };
+console.log('./db');
+
+// clear
