c947f4a3c60b6c2b3d1433dc075a34e74f11f8cd
Lucas <lmaul77@gmail.com>
2022-08-17 10:36:48 -0500

    fixedshop

--- api/products.js
+++ api/products.js
@@ -23,7 +23,7 @@ router.get("/", async (req, res) => {
 });
 
 
-router.get("/:category", async (req, res) => {
+router.get("/category/:category", async (req, res) => {
   try {
     const { category } = req.params;
     const products = await getProductByCategory(category);
--- src/components/Longsleeve.js
+++ src/components/Longsleeve.js
@@ -52,15 +52,17 @@ const Longsleeve = ({allProducts, setAllProducts}) => {
             allProducts.map((element, idx) => {
                 return(
                     <div className="LongSleeveContainer">
-        <div>{element.product_name}</div>
+        <div><b>{element.product_name}</b></div>
         <img className="LongSleeveImage" src={LongSleeve} />
         <div className="LongSleeveInfoContainer">
-                    <div>Gender: {element.gender}</div>
-                    <div>Category: {element.category}</div>
-                    <div>Size: {element.size}</div>
-                    <div>Price: {element.price}</div>
-                    <div>InStock?: {element.quantity_instock}</div>
-                    <div>Description: {element.description}</div>
+                    <div><b>Gender: </b>{element.gender}</div>
+                    <div><b>Category: </b>{element.category}</div>
+                    <div><b>Size: </b>{element.size}</div>
+                    <div><b>Price: </b>{element.price}</div>
+                    <div><b>InStock?: </b>{element.quantity_instock}</div>
+                    <p className="LongSleeveAbout">
+                    <div><b>Description: </b>{element.description}</div>
+                    </p>
                     <form>
                     <button type="Submit" className="LongSleeveButton">Add to Cart</button>
                     </form>
