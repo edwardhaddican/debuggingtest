190d89bf1bcdf605929930273a4b0d192d59eab1
Tanner <tannerazm@yahoo.com>
2022-08-24 08:18:34 -0600

    package json and port changes (4000)

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 const {app} = require('./app');
 
-const PORT = process.env['PORT'] ?? 3001;
+const PORT = process.env['PORT'] ?? 4000;
 
 
 app.listen(PORT, async () => {
--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const BASE = "http://localhost:3001";
+const BASE = "http://localhost:4000";
 
 // USERS API CALLS BELOW
 
