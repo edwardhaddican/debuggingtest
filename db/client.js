b13921b3105abf5c38c46b6237e2ca19628ddfc3
Tanner <tannerazm@yahoo.com>
2022-08-24 07:54:13 -0600

    heroku debugging

--- db/client.js
+++ db/client.js
@@ -1,11 +1,13 @@
-const { Client} = require('pg');
+const { Client } = require('pg');
 
 const connectionString =
   process.env.DATABASE_URL || 'https://localhost:5432/topsecret';
 
 const client = new Client({
   connectionString,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
+
 client.connect()
 
 module.exports = client;
